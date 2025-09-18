feb63ba636dd37123190142a2a903d48c9586063
thayna09as <thayna.amaral.silva@escola.pr.gov.br>
2025-09-18 11:24:10 -0300

    .

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-
+var nomeCantina = "Cantina da Escola";
+console.log("Bem-vindo à " + nomeCantina);
+disponíveis
+let salgados = 20;
+console.log("Temos " + salgados + " salgados disponíveis.");
+const precoSalgado = 5;
+console.log("Cada salgado custa R$" + precoSalgado);
+console.log("Agora restam " + salgados + " salgados.");
+console.log("A cantina vendeu R$" + totalVendido);
+if (true) {
+var testeVar = "Sou var"; 
+ let testeLet = "Sou let"; 
+ console.log(testeVar); // funciona 
+ console.log(testeLet); // funciona
+} 
+console.log(testeVar); // funciona 
+console.log(testeLet); // ERRO
